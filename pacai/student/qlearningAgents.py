812e8ca8b1f00ad1e0a3b61ee0461268d2539f09
Lilian Gallon <swellchaser33@gmail.com>
2019-11-15 17:59:29 -0800

    P3: question 4: Q-Learning

--- pacai/student/qlearningAgents.py
+++ pacai/student/qlearningAgents.py
@@ -1,5 +1,9 @@
+import random
+
 from pacai.agents.learning.reinforcement import ReinforcementAgent
 from pacai.util import reflection
+from pacai.util import counter
+from pacai.util import probability
 
 class QLearningAgent(ReinforcementAgent):
     """
@@ -45,7 +49,7 @@ class QLearningAgent(ReinforcementAgent):
     def __init__(self, index, **kwargs):
         super().__init__(index, **kwargs)
 
-        # You can initialize Q-values here.
+        self.qValues = counter.Counter()
 
     def getQValue(self, state, action):
         """
@@ -54,7 +58,8 @@ class QLearningAgent(ReinforcementAgent):
         Should return 0.0 if the (state, action) pair has never been seen.
         """
 
-        return 0.0
+        # If the (state, action) pair has never been seen then it is 0 by default (see init()).
+        return self.qValues[(state, action)]
 
     def getValue(self, state):
         """
@@ -69,7 +74,8 @@ class QLearningAgent(ReinforcementAgent):
         Whereas this method returns the value of the best action.
         """
 
-        return 0.0
+        value = self.__getBestActionAndValue(state)[1]
+        return value if value != - float("inf") else 0
 
     def getPolicy(self, state):
         """
@@ -84,7 +90,81 @@ class QLearningAgent(ReinforcementAgent):
         Whereas this method returns the best action itself.
         """
 
-        return None
+        actions = self.__getBestActionAndValue(state)[0]
+        return random.choice(actions) if actions is not None else None
+
+    def getAction(self, state):
+        """
+        Compute the action to take in the current state.
+        With probability `pacai.agents.learning.reinforcement.ReinforcementAgent.getEpsilon`,
+        we should take a random action and take the best policy action otherwise.
+        Note that if there are no legal actions, which is the case at the terminal state,
+        you should choose None as the action.
+        """
+
+        action = None
+        legalActions = self.getLegalActions(state)
+
+        # We have to make sure that there are some legal actions
+        if legalActions:
+            if probability.flipCoin(self.getEpsilon()):
+                # In this case we take the random action
+                action = random.choice(legalActions)
+            else:
+                # Otherwise we take the best one
+                action = self.getPolicy(state)
+
+        return action
+
+    def update(self, state, action, nextState, reward):
+        """
+        The parent class calls this to observe a state transition and reward.
+        You should do your Q-Value update here.
+        Note that you should never call this function, it will be called on your behalf.
+        """
+
+        # Okay, so we don't want to return anything
+        # 1) We just need to update the qValues
+        # 2) Now we have to know HOW to update these values
+        # 3) After taking a look at the wonderful lecture material, we know the formula:
+        # V(s) = (1 - alpha) * V(s) + alpha * [R(s, policy(s), s') + lamda * V(s')]
+        # 4) In the commens we can see that the function will be called on our behalf, so now we
+        # have everything to code
+
+        a = self.getAlpha()
+        vs = self.getQValue(state, action)  # vs stands for V(s)
+        vss = self.getValue(nextState)  # vss stands for V(s')
+        drate = self.getDiscountRate()
+
+        self.qValues[(state, action)] = (1 - a) * vs + a * (reward + drate * vss)
+
+    def __getBestActionAndValue(self, state):
+        """
+        We will use this code multiple time, so we will use only one function to do so.
+        returns a pair of ([list of best action], best value) from the state
+        It returns (None, - float("inf")) if we are in a terminal state
+
+        PS: it is the same function as the one in valueIterationAgent BUT it returns an array of
+        possible actions.
+        """
+
+        # We need to compute V(s) = (a)max(Q(s, a))
+        # In brief, we want the qValue for the best action
+
+        actions = self.getLegalActions(state)
+        bestActions = []  # it will happen if we are in the terminal state
+        bestQValue = - float("inf")
+
+        for action in actions:
+            qValue = self.getQValue(state, action)
+
+            if qValue > bestQValue:
+                bestQValue = qValue
+                bestActions = [action]
+            elif qValue == bestQValue:  # I spent more time on that than I would admit
+                bestActions.append(action)
+
+        return bestActions if bestActions else None, bestQValue
 
 class PacmanQAgent(QLearningAgent):
     """
